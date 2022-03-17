9626889c5b76526e840df96e8ad781a38d7c8ab2
marikcion <marikcion@gmail.com>
2022-03-17 13:05:42 +0300

    проверка страницы

--- main.rb
+++ main.rb
@@ -1,8 +1,22 @@
-require_relative 'class_get.rb'
+require './class_get.rb'
+require './chek.rb'
+require './parser.rb'
+require 'logger'
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
 
 
 url = ARGV[0]
-url_response = Zapros.new(url)
+url_response = Zapros.new()
+html = url_response.get_requsts(url).body_str
+
+ch = Chek.new()
+res = ch.chek(html).to_s
+if res == '<a href="/candies/?page=2" class="g-pagination__list__item">2</a>'
+    puts "В переменной лежит значение #{res} "
+else
+    puts "В переменной не лежит значение"
+end
+
+
