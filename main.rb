b0692a4379163a990a77c16b0185ee515d4bb19e
marikcion <marikcion@gmail.com>
2022-03-18 11:09:57 +0300

    логика программы

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require './class_get.rb'
+require './class_save.rb'
 require './chek.rb'
 require './parser.rb'
 require 'logger'
@@ -6,15 +7,20 @@ require 'logger'
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
 
+#pars = Parser.new()
 
 url = ARGV[0]
-url_response = Zapros.new()
-html = url_response.get_requsts(url).body_str
+
+response1 = Zapros.new()
+html = response1.get_requsts(url).body_str
 
 ch = Chek.new()
 res = ch.chek(html).to_s
 if res == '<a href="/candies/?page=2" class="g-pagination__list__item">2</a>'
-    puts "В переменной лежит значение #{res} "
+    response2 = Zapros.new()
+    res = response2.get_requsts(url).body_str
+    save = Save.new()
+    save.save_file(res)  
 else
     puts "В переменной не лежит значение"
 end
