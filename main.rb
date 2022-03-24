8e66b8ed4da1c386dc45184d7ed9983227031094
marikcion <marikcion@gmail.com>
2022-03-24 13:00:29 +0300

    главный файл

--- main.rb
+++ main.rb
@@ -1,28 +1,16 @@
 require './class_get.rb'
 require './class_save.rb'
-require './chek.rb'
-require './parser.rb'
+require './class_parser.rb'
 require 'logger'
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
 
-#pars = Parser.new()
-
 url = ARGV[0]
 
 response1 = Zapros.new()
 html = response1.get_requsts(url).body_str
-
-ch = Chek.new()
-res = ch.chek(html).to_s
-if res == '<a href="/candies/?page=2" class="g-pagination__list__item">2</a>'
-    response2 = Zapros.new()
-    res = response2.get_requsts(url).body_str
-    save = Save.new()
-    save.save_file(res)  
-else
-    puts "В переменной не лежит значение"
-end
-
+pars = Parser.new()
+res= pars.chek(html)
+puts res
 
